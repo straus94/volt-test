7e65c7d06c4ab98be8a00f146821fc9d375b4796
Denm <straus94@gmail.com>
2024-03-05 12:40:40 +0200

    setup project

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+  },
+  'extends': [
+    '@wemake-services/typescript/recommended',
+  ],
+  'overrides': [
+    {
+      'env': {
+        'node': true,
+      },
+      'files': [
+        '.eslintrc.{js,cjs}',
+      ],
+      'parserOptions': {
+        'sourceType': 'script',
+      },
+    },
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module',
+  },
+  'plugins': [
+    '@typescript-eslint',
+    'react',
+  ],
+  'rules': {},
+}
